2bc13746125e2f4ddae6690cf0e198e560cecdba
Stefan Genchev <stefan.genchev@sgsites.net>
2018-06-13 16:56:26 +0300

    Replace sample authorization details in test application with more general ones

--- LogSentinel.Client.Test/Program.cs
+++ LogSentinel.Client.Test/Program.cs
@@ -8,8 +8,8 @@ namespace LogSentinel.Client.Test
         public static void LogEntry()
         {
             LogSentinelClientBuilder builder = LogSentinelClientBuilder
-                .create("38773350-6a64-11e8-a7b3-cfa432063561", "387648f0-6a64-11e8-a7b3-cfa432063561",
-                "bedaac15925a2e46eed80d5b5276fd91764d3ea4fb5ac2b44bd6e06e4c83be87");
+                .create("applicationId", "organizationId",
+                "secret");
 
             builder.setEncryptionKey(Convert.FromBase64String("NWFhMTQ0YWI3ZmU5YTJmNDk2MmVhZmU2ZTFkZWUzNWQ="));
 
@@ -33,7 +33,7 @@ namespace LogSentinel.Client.Test
                         .setActorId(logEntry.ActorId),
                     new ActionData().setDetails(logEntry.ActionDetails).setAction(logEntry.Action)
                         .setEntryType(logEntry.EntryType), 
-                    "38773350-6a64-11e8-a7b3-cfa432063561"
+                    "applicationId"
                 );
 
                 Console.WriteLine(result.LogEntryId);
